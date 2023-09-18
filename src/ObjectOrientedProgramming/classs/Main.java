ebb3a655348da62b479e3e4d558df37f512ebeb1
Hywenklis <hywenklis@hotmail.com>
2023-09-18 20:43:13 -0300

    5.5. Acessando atributos de objetos

--- src/ObjectOrientedProgramming/classs/Main.java
+++ src/ObjectOrientedProgramming/classs/Main.java
@@ -5,6 +5,28 @@ public class Main {
     public static void main(String[] args) {
 
         Car myCar = new Car();
+        myCar.manufacturingYear = 2021;
+        myCar.color = "Black";
+        myCar.manufacturer = "Honda";
+        myCar.model = "HR-V";
+
         Car yourCar = new Car();
+        yourCar.manufacturingYear = 2022;
+        yourCar.color = "White";
+        yourCar.manufacturer = "BMW";
+        yourCar.model = "X6";
+
+        System.out.println("My car");
+        System.out.println("--------");
+        System.out.printf("Model: %s%n", myCar.model);
+        System.out.printf("Year: %d%n", myCar.manufacturingYear);
+
+        System.out.println();
+
+        System.out.println("Your car");
+        System.out.println("--------");
+        System.out.printf("Model: %s%n", yourCar.model);
+        System.out.printf("Year: %d%n", yourCar.manufacturingYear);
+
     }
 }
