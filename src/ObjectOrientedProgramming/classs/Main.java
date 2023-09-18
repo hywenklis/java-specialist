3bb4c731f9ce4b11a6c088309ae821b2ca1b105c
Hywenklis <hywenklis@hotmail.com>
2023-09-18 20:37:23 -0300

    5.4. Instanciando objetos

---
+++ src/ObjectOrientedProgramming/classs/Main.java
@@ -0,0 +1,10 @@
+package ObjectOrientedProgramming.classs;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car myCar = new Car();
+        Car yourCar = new Car();
+    }
+}
