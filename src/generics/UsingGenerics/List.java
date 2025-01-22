ea67f3a432be903bb9aec4a550460ae5ded15d08
hywenklis <hywenklis@hotmail.com>
2025-01-22 19:13:45 -0300

    Utilizando generics

---
+++ src/generics/UsingGenerics/List.java
@@ -0,0 +1,15 @@
+package generics.UsingGenerics;
+
+import java.util.ArrayList;
+
+public class List<T> {
+    private java.util.List<T> list = new ArrayList<>();
+
+    public void add(T element) {
+        list.add(element);
+    }
+
+    public T get(int i) {
+        return list.get(i);
+    }
+}
---
+++ src/generics/UsingGenerics/ListWithoutGenerics.java
@@ -0,0 +1,19 @@
+package generics.UsingGenerics;
+
+public class ListWithoutGenerics {
+    public static void main(String[] args) {
+        List<Integer> numbers = new List<>();
+        numbers.add(1);
+        numbers.add(2);
+
+        List<String> texts = new List<>();
+        texts.add("abc");
+        texts.add("cba");
+
+        int number = numbers.get(0);
+        System.out.println(number);
+
+        String text = texts.get(0);
+        System.out.println(text);
+    }
+}
